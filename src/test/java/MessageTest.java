7bbdaa7f7c432900d4ac7267d35a368bb2e8c1e4
entle-ngqeku <st10538239@rcconnect.edu.za>
2026-05-25 14:24:41 +0200

    test: Added LoginTest and MessageTest-all unit tests passing

---
+++ src/test/java/MessageTest.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+import com.mycompany.chatapp2.Message;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Student
+ */
+
+public class MessageTest {
+    
+    @Test
+    public void testMessageLengthValid() {
+        Message msg = new Message("+27718693002", "Hi there", 1);
+        assertEquals("Message ready to send.", msg.checkMessageLength());
+    }
+    
+    @Test
+    public void testMessageLengthInvalid() {
+        String longMsg = "A".repeat(260); // 260 characters
+        Message msg = new Message("+27718693002", longMsg, 1);
+        assertEquals("Message exceeds 250 characters by 10; please reduce the size.", 
+                     msg.checkMessageLength());
+    }
+    
+    @Test
+    public void testRecipientValid() {
+        Message msg = new Message("+27718693002", "Test", 1);
+        assertEquals("Cell phone number successfully captured.", 
+                     msg.checkRecipientCell());
+    }
+    
+    @Test
+    public void testRecipientInvalid() {
+        Message msg = new Message("08575975889", "Test", 1);
+        assertEquals("Cell phone number is incorrectly formatted or does not contain an international code. Please correct the number and try again.",
+                     msg.checkRecipientCell());
+    }
+    
+    @Test
+    public void testMessageHashCorrect() {
+        // Test data: ID starts with "00", number 0, first "HI", last "NIGHT"
+        Message msg = new Message("+27718693002", "Hi Mike, can you join us for dinner tonight?", 0);
+        // Hash should be: 00:0:HITONIGHT
+        assertEquals("00:0:HITONIGHT", msg.createMessageHash());
+    }
+    
+    @Test
+    public void testMessageIDExists() {
+        Message msg = new Message("+27718693002", "Test", 1);
+        assertTrue(msg.checkMessageID());
+        assertEquals(10, msg.getMessageID().length());
+    }
+}
